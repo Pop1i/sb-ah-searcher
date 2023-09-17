8205ba2c6eb02b034572f162186aefb5ddfa6bcd
Pop_i <94174441+Pop1i@users.noreply.github.com>
2023-09-17 12:18:05 +0200

    added colors

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import requests
+import colorama
+
+colorama.init(autoreset=True)
 
 search = input("item to search for: ")
 
@@ -17,7 +20,23 @@ for item in auctions:
     if item["bin"] == True:
         item_name = item["item_name"]
         if search.lower() in item_name.lower():
+            item_rarity = item["tier"]
+
             item_price = item["starting_bid"]
-            item_price = "{:,}".format(item_price)
+            item_price = colorama.Fore.YELLOW + "{:,}".format(item_price)
+
+            match item_rarity:
+                case "COMMON":
+                    item_name = colorama.Fore.WHITE + item_name
+                case "UNCOMMON":
+                    item_name = colorama.Fore.GREEN + item_name
+                case "RARE":
+                    item_name = colorama.Fore.BLUE + item_name
+                case "EPIC":
+                    item_name = colorama.Fore.MAGENTA + item_name
+                case "LEGENDARY":
+                    item_name = colorama.Fore.YELLOW + item_name
+                case "SPECIAL":
+                    item_name = colorama.Fore.RED + item_name
 
             print(f"{item_name} : {item_price}")
